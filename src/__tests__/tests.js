6c605acf3fc4f3b9cbb8a5e8237f82e46cfd4534
tthais <thaisatamar@gmail.com>
2022-02-17 10:43:32 +0200

    add app

---
+++ src/__tests__/tests.js
@@ -0,0 +1,9 @@
+describe("Calculator tests", () => {
+    test('adding 1 + 2 should return 3', () => {
+        // arrange and act
+        const result = 1+2;
+
+        // assert
+        expect(result).toBe(3);
+    });
+})
